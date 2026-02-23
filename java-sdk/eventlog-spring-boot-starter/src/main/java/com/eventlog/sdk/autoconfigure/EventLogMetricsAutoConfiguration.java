882d86223e033ccf56cc688c4a04d281a3bc7e85
Srinath Venkatesh <srinathvb1@gmail.com>
2026-02-23 18:12:41 -0500

    event log metrics fix for pets-api

--- java-sdk/eventlog-spring-boot-starter/src/main/java/com/eventlog/sdk/autoconfigure/EventLogMetricsAutoConfiguration.java
+++ java-sdk/eventlog-spring-boot-starter/src/main/java/com/eventlog/sdk/autoconfigure/EventLogMetricsAutoConfiguration.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
-@AutoConfiguration(after = EventLogAutoConfiguration.class)
+@AutoConfiguration(
+        after = EventLogAutoConfiguration.class,
+        afterName = "org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration"
+)
 @ConditionalOnClass(MeterRegistry.class)
 @ConditionalOnBean({AsyncEventLogger.class, MeterRegistry.class})
 @ConditionalOnProperty(prefix = "eventlog.metrics", name = "enabled", havingValue = "true", matchIfMissing = true)
